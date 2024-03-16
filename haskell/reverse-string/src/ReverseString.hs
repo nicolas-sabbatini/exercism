d84a6f94050a05c382ca7679fb3c53beace9072c
Nicolas Sabbatini <nik.code.things@gmail.com>
2024-03-16 00:53:15 -0300

    haskell/reverse-string

---
+++ haskell/reverse-string/src/ReverseString.hs
@@ -0,0 +1,8 @@
+module ReverseString (reverseString) where
+
+reverseString :: String -> String
+reverseString = reverse
+
+-- reverseString :: String -> String
+-- reverseString [] = []
+-- reverseString (x:xs) = reverseString xs ++ [x]
